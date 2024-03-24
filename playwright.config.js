0f0c8060c61a834e82686472a82ddeba7ac86eaa
Hammad Ahmed <hammadahmedkhan89@gmail.com>
2024-03-24 02:54:00 +0100

    updated karachi appointment

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testDir: "./tests",
   use: {
     headless: false,
-    trace: process.env.CI ? "off" : "on",
+    trace: process.env.CI ? "retain-on-failure" : "on",
     video: process.env.CI ? "off" : "on", // disabled on ci for performance reasons, use trace viewer instead
     screenshot: process.env.CI ? "off" : "on", //
   },
